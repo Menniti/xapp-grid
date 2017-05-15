8a39eebb5581745b5dea2ac9d58489b836567f1f
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-05-14 21:54:27 -0300

    add server.js middle were and configurations

---
+++ server.js
@@ -0,0 +1,37 @@
+// server.js
+
+// call packages
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// config app to use bodyParser()
+// get data from post
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// set port
+var port = process.env.PORT || 8080;
+
+// ROUTES API
+//=====================================
+// get a instance of express Router
+var router = express.Router();
+
+// route test
+router.get('/', function(req, res){
+	res.json({message: 'Server working!'});
+});
+
+// routes
+
+
+// REGISTER ROUTES -------------
+// routes PREFIX
+app.use('/api', router);
+
+// START SERVER
+// ==============================
+app.listen(port);
+console.log('Listen on port' + port);
